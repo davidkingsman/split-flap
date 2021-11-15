cb917bf7b1a7a48ed542e4f6d4b0776cce18c8fa
Kurt Beheydt <kurt@postduif.be>
2021-11-15 23:19:55 +0100

    backend in socket io with test buttons

--- backend/public/index.js
+++ backend/public/index.js
@@ -1,3 +1,72 @@
-$(document).ready(function () {
+var client = {
+  id: false,
+  alignment: "left", // left|center|right
+  speedSlider: 50, // 1-100
+  deviceMode: "text", // text|date|clock
+  inputText: ""
+};
 
+var sendClient = function (message, data = false) {
+  socket.emit("clientRequest", {
+    message: message,
+    data: data
+  });
+};
+
+var getValues = function () {
+  socket.emit("clientRequest", "getValues|all");
+};
+
+var setSpeedSlider = function (speedSlider) {
+  socket.emit("clientRequest", "setSpeedSlider|" + speedSlider);
+};
+
+var setAlignment = function (alignment) {
+  socket.emit("clientRequest", "setAlignment|" + alignment);
+};
+
+var setDeviceMode = function (deviceMode) {
+  socket.emit("clientRequest", "setDeviceMode|" + deviceMode);
+};
+
+var setInputText = function (inputText) {
+  socket.emit("clientRequest", "setInputText|" + inputText);
+};
+
+$(document).ready(function () {
+  socket.emit("identifyAdmin", "jos");
+
+  socket.on("message", function (payload) {
+    console.log("message");
+    console.log(payload);
+
+    if (payload.message == "clientDetails") {
+      client.id = payload.clientid;
+      getValues();
+    } else if (payload.message == "valuesUpdate") {
+      client.alignment = payload.data.alignment;
+      client.speedSlider = payload.data.speedSlider;
+      client.deviceMode = payload.data.deviceMode;
+      client.inputText = payload.data.inputText;
+    }
+  });
+
+  //setDeviceMode("text");
+  //setInputText("A");
+
+  $("body").on("click", "#setText", function () {
+    console.log("set text");
+    setDeviceMode("text");
+  });
+  $("body").on("click", "#setK", function () {
+    console.log("set K");
+    setInputText("K");
+  });  $("body").on("click", "#setJ", function () {
+    console.log("set J");
+    setInputText("J");
+  });
+  $("body").on("click", "#setClock", function () {
+    console.log("set clock");
+    setDeviceMode("clock");
+  });
 });
--- backend/server.js
+++ backend/server.js
@@ -10,26 +10,111 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
+var port = process.env.PORT || 3100;
+
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 
-io.on("connection", function (socket) {
-  var connId = false;
+var webSockets = {};
+var clients = {};
+var admins = {};
 
-  if (socket.conn) {
-    connId = socket.conn.id;
+// methods
+var getKeyByValue = function (object, value) {
+  return Object.keys(object).find(key => object[key] === value);
+};
+
+var getClientForClientConnId = function (clientConnId) {
+  var client = getKeyByValue(clients, clientConnId);
+  return client;
+};
+
+var getAdminForAdminConnId = function (adminConnId) {
+  var admin = getKeyByValue(admins, adminConnId);
+  return admin;
+};
+
+var getConnIdForClient = function (client) {
+  var connId = clients[client];
+
+  if (connId < 0) {
+    connId = false;
   }
 
-  var message = "a client connected: " + connId;
-  console.log(message);
+  console.log(connId, "for", client);
 
-  socket.on("disconnect", function (msg) {
-    var message = "a client disconnected: " + connId;
-    console.log(message);
+  return connId;
+};
+
+var getClientConnIdForAdminConnId = function (adminConnId) {
+  var admin = getKeyByValue(admins, adminConnId);
+  var connId = clients[admin];
+  //console.log(adminConnId, admin, connId);
+  return connId;
+};
+
+io.on("connection", function (webSocket) {
+  var connId = false;
+
+  if (webSocket.conn) {
+    connId = webSocket.conn.id;
+  }
+
+  webSockets[connId] = webSocket;
+  console.log("a client connected: " + connId);
+
+  webSocket.on("disconnect", function (msg) {
+    delete webSockets[connId];
+    delete getClientForClientConnId(connId);
+    delete getAdminForAdminConnId(connId);
+
+    console.log("a client disconnected: " + connId);
+  });
+
+  webSocket.on("identify", function (data) {
+    clients[data] = connId;
+
+    if (admins[data]) {
+      webSockets[admins[data]].send({message: "clientDetails", clientId: connId});
+    }
+
+    console.log("identify from", connId, ":", data);
+  });
+
+  webSocket.on("identifyAdmin", function (data) {
+    admins[data] = connId;
+
+    var clientId = clients[data]
+      ? clients[data]
+      : 0;
+
+    webSockets[connId].send({message: "clientDetails", clientId: clientId});
+
+    console.log("identify admin from", connId, ":", data);
+  });
+
+  webSocket.on("valuesUpdate", function (data) {
+    console.log("valuesUpdate");
+    console.log(data);
+    var clientId = admins[getClientForClientConnId(connId)];
+
+    if (clientId) {
+      webSockets[clientId].send({message: "valuesUpdate", data: data});
+    }
+  });
+  
+
+  webSocket.on("clientRequest", function (data) {
+    console.log("clientRequest");
+    console.log(data);
+
+    var admin = getAdminForAdminConnId(connId);
+
+    if (clients[admin]) {
+      webSockets[clients[admin]].send(data);
+    }
   });
 });
-
-var port = process.env.PORT || 3100;
 
 http.listen(port, function () {
   console.log("listening on *:" + port);
