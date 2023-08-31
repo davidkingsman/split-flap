483ff483d0804285c5468a782d35158fa96b2b73
Jonny Booker <1131478+JonnyBooker@users.noreply.github.com>
2023-08-31 19:43:34 +0100

    Updated string alignment methods
    - Left align string clears off any text to the right
    - Right align string implementation added
    - Center string when provided a long string, then subsequently a short string, did not clear the right hand side of the string

--- ESPMaster/stringHandling.ino
+++ ESPMaster/stringHandling.ino
@@ -1,38 +1,43 @@
-
+//aligns string center by filling the left and right with spaces up the max of the units amount
 String centerString(String message) {
-  String emptySpace;
-  int messageLength = message.length();
-  int emptySpaceAmount = (UNITSAMOUNT - messageLength) / 2;
-  for (int i = 0; i < emptySpaceAmount; i++) {
-    emptySpace = " " + emptySpace;
+  //Takes care of the left side of the text (if any)
+  int leftSpaceAmount = (UNITSAMOUNT -  message.length()) / 2;
+  for (int i = 0; i < leftSpaceAmount; i++) {
+    message = " " + message;
   }
-  message = emptySpace + message;
+
+  //Take care of the right side of the text (if any)
+  for(int i = message.length(); i < UNITSAMOUNT; i++) {
+    message = message + " ";
+  }
+
   message = cleanString(message);
+  
   return message;
 }
 
+//aligns string on right side of array and fills empty chars with spaces
 String rightString(String message) {
+  int rightSpaceAmount = (UNITSAMOUNT - message.length());
+  for (int i = 0; i < rightSpaceAmount; i++) {
+    message = " " + message;
+  }
+
   message = cleanString(message);
+
   return message;
 }
 
 //aligns string on left side of array and fills empty chars with spaces
 String leftString(String message) {
+  int leftSpaceAmount = (UNITSAMOUNT - message.length());
+  for (int i = 0; i < leftSpaceAmount; i++) {
+    message = message + " ";
+  }
+
   message = cleanString(message);
 
-  char leftAlignString[UNITSAMOUNT + 1];
-
-  for (unsigned int i = 0; i < UNITSAMOUNT + 1; i++) {
-    if (i < message.length()) {
-      leftAlignString[i] = message[i];
-    } else if (i == UNITSAMOUNT) {
-      leftAlignString[i] = '\0';
-    } else {
-      leftAlignString[i] = ' ';
-    }
-  }
-  //Serial.println(leftAlignString);
-  return leftAlignString;
+  return message;
 }
 
 //converts input string to uppercase
